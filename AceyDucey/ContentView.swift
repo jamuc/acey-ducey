3352a421c0a12fdcb9da0ac5315f374dccdcdf47
Jason Franklin <jason.franklin@xing.com>
2020-04-12 15:12:24 +0200

    Initial Commit

---
+++ AceyDucey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AceyDucey
+//
+//  Created by Jason Franklin on 12.04.20.
+//  Copyright © 2020 Jason Franklin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
